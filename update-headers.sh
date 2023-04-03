1e5fd1be03f6fb6175e3ba946855dfccff1e8de2
LordMZTE <lord@mzte.de>
2023-04-03 23:38:05 +0200

    generate wayland-version.h

--- update-headers.sh
+++ update-headers.sh
@@ -3,6 +3,22 @@ set -ex
 
 rm include/* || true
 
+# Clone Wayland repo
 git clone https://gitlab.freedesktop.org/wayland/wayland.git --depth 1
+
+# Move headers
 mv wayland/src/*.h include
+
+# Generate version header
+version=$(sed -n 's/^\s*version: *'\''\(.*\)'\'',$/\1/p' wayland/meson.build)
+parts=(${version//./ })
+
+sed \
+    -e "s/@WAYLAND_VERSION@/${version}/" \
+    -e "s/@WAYLAND_VERSION_MAJOR@/${parts[0]}/" \
+    -e "s/@WAYLAND_VERSION_MINOR@/${parts[1]}/" \
+    -e "s/@WAYLAND_VERSION_MICRO@/${parts[2]}/" \
+    wayland/src/wayland-version.h.in >include/wayland-version.h
+
+# Remove wayland repo
 rm -rf wayland
